f00c4f0f039580fe7b3651735c089af7045ad984
Jany <my.maithi@gmail.com>
2016-06-12 21:00:50 +0700

    Get images

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,7 +1,22 @@
+getImages();
+
 // Add text
 function addText() {
   var elTextCanvas = document.getElementById('textCanvas');
   var elCanvasBlock = document.getElementById('blockCanvas');
 
   Action.addText(elTextCanvas, elCanvasBlock);
+}
+
+
+// Get images
+function getImages() {
+  Media.getImages("/images")
+    .then(function(response) {
+      var elParent = document.getElementById('listImage');
+      var elChild = document.createElement('div');
+      Media.getImagesSuccessful(response, elChild, elParent);
+    }, function(error) {
+      alert(error);
+    });
 }
